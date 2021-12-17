4f6f91ed8c1aae4a2baf359bf3a6fd83955af881
Guilherme Sionek <gsionek@gmail.com>
2021-12-16 23:16:44 -0300

    Running CanDevice::readFrame on separate thread.

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,39 @@
 #include <string.h>
 #include <iostream>
+#include <thread>
 #include "candevice.hpp"
 #include "canframe.hpp"
+
+static bool isFinished = false;
 
 void print(const char* message)
 {
     std::cout << message << std::endl;
 }
 
-int main()
+void startCanReader()
 {
-    print("Starting CanViewer");
     CanDevice can_device("vcan0");
 
-    // TODO: use std::thread and wait for user input to .join()
-    while (true)
+    while (!isFinished)
     {
         CanFrame frame;
         frame = can_device.readFrame();
         frame.print();
         // std::cout << frame << std::endl;
     }
+}
+
+int main()
+{
+    print("Starting CanViewer");
+
+    std::thread worker(startCanReader);
+    
+    std::cin.get();
+    isFinished = true;
+
+    worker.join();    
 
     print("Shutting down... Goodbye!");
     return 0;
